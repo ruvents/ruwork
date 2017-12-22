3736f4d97061a9c18ce767a09eafa6f96240ccaf
Valentin <udaltsov.valentin@gmail.com>
2017-12-22 22:49:06 +0300

    [Ruwork] Added splitsh-lite examples to push.sh.

--- push.sh
+++ push.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-git subtree push --prefix src/AdminBundle git@github.com:ruvents/ruwork-admin-bundle.git master
-git subtree push --prefix src/ApiBundle git@github.com:ruvents/ruwork-api-bundle.git master
-git subtree push --prefix src/DoctrineBehaviorsBundle git@github.com:ruvents/ruwork-doctrine-behaviors-bundle.git master
-git subtree push --prefix src/DoctrineFilterBundle git@github.com:ruvents/ruwork-doctrine-filter-bundle.git master
-git subtree push --prefix src/DoctrinePostgresqlBundle git@github.com:ruvents/ruwork-doctrine-postgresql-bundle.git master
-git subtree push --prefix src/LocalePrefixBundle git@github.com:ruvents/ruwork-locale-prefix-bundle.git master
-git subtree push --prefix src/ManualAuthBundle git@github.com:ruvents/ruwork-manual-auth-bundle.git master
-git subtree push --prefix src/Paginator git@github.com:ruvents/ruwork-paginator.git master
-git subtree push --prefix src/PaginatorBundle git@github.com:ruvents/ruwork-paginator-bundle.git master
-git subtree push --prefix src/UploadBundle git@github.com:ruvents/ruwork-upload-bundle.git master
+#git push git@github.com:vudaltsov/mini-a.git `splitsh-lite --prefix=src/a`:master
+#git push git@github.com:vudaltsov/mini-b.git `splitsh-lite --prefix=src/b`:master

dbf6a77b2460644cc9e133eb5cb6d3b0ee81d676
Valentin <udaltsov.valentin@gmail.com>
2017-12-23 03:37:20 +0300

    [Ruwork] Renamed push.sh -> publish and added ManualAuthBundle.

--- push.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#git push git@github.com:vudaltsov/mini-a.git `splitsh-lite --prefix=src/a`:master
-#git push git@github.com:vudaltsov/mini-b.git `splitsh-lite --prefix=src/b`:master

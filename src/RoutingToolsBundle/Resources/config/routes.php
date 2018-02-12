288e2fe76801b8f46c7b0e824c0be2b6c849745f
Valentin <udaltsov.valentin@gmail.com>
2018-02-13 00:03:02 +0300

    [RoutingToolsBundle] Fixed remove_trailing_slash route definition.

--- src/RoutingToolsBundle/Resources/config/routes.php
+++ src/RoutingToolsBundle/Resources/config/routes.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Symfony\Component\Routing\Loader\Configurator;
 
-use Ruwork\RoutingToolsBundle\Controller\RemoveTrailingSlashController;
-
 return function (RoutingConfigurator $configurator): void {
     $configurator
         ->add('ruwork_routing_tools.remove_trailing_slash', '/{path}')
-        ->controller(RemoveTrailingSlashController::class)
+        ->controller('ruwork_routing_tools.controller.remove_trailing_slash')
         ->requirements([
             'path' => '.*\/$',
         ]);

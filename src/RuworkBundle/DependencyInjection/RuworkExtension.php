1c1baf424ef45cd0f4cc0a7e9f921e0b9df107c5
Valentin <udaltsov.valentin@gmail.com>
2018-02-16 21:29:06 +0300

    [RuworkBundle] Removed I18nControllerTemplateListener and its config.

--- src/RuworkBundle/DependencyInjection/RuworkExtension.php
+++ src/RuworkBundle/DependencyInjection/RuworkExtension.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ruwork\RuworkBundle\DependencyInjection;
 
-use Ruwork\RuworkBundle\EventListener\I18nControllerTemplateListener;
 use Ruwork\RuworkBundle\Mailer\Mailer;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -20,16 +19,6 @@ final class RuworkExtension extends ConfigurableExtension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
-
-        if ($config['i18n']['enabled']) {
-            if ($config['i18n']['suffix_controller_templates']) {
-                $container->autowire(I18nControllerTemplateListener::class)
-                    ->setAutoconfigured(true)
-                    ->setPublic(false)
-                    ->setArgument('$locales', $config['i18n']['locales'])
-                    ->setArgument('$defaultLocale', $config['i18n']['default_locale']);
-            }
-        }
 
         $container->findDefinition(Mailer::class)
             ->setArgument('$users', $config['mailer']['users']);

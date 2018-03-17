d40cb647be35a86465e14d3bb32e351775855dee
Valentin <udaltsov.valentin@gmail.com>
2018-03-18 00:40:05 +0300

    [RuworkBundle] Added UserTrait.

---
+++ src/RuworkBundle/Security/UserTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ruwork\RuworkBundle\Security;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+trait UserTrait
+{
+    /**
+     * @see UserInterface::getRoles()
+     */
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
+    }
+
+    /**
+     * @see UserInterface::getPassword()
+     */
+    public function getPassword()
+    {
+        return '';
+    }
+
+    /**
+     * @see UserInterface::getSalt()
+     */
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface::eraseCredentials()
+     */
+    public function eraseCredentials()
+    {
+    }
+}

cf65784b2c0b2bc28c1395c43589d43b9dd13f18
Valentin <udaltsov.valentin@gmail.com>
2018-06-04 00:46:20 +0200

    [FrujaxBundle] Added FrujaxBlockAwareRenderer service definition.

--- src/FrujaxBundle/Resources/config/services.php
+++ src/FrujaxBundle/Resources/config/services.php
@@ -7,18 +7,21 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use Ruwork\FrujaxBundle\EventListener\FrujaxAnnotationListener;
 use Ruwork\FrujaxBundle\EventListener\FrujaxRedirectListener;
 use Ruwork\FrujaxBundle\EventListener\FrujaxResponseListener;
+use Ruwork\FrujaxBundle\Twig\FrujaxBlockAwareRenderer;
 
 return function (ContainerConfigurator $container): void {
     $services = $container->services()
         ->defaults()
         ->private();
 
-    $services->set('ruwork_frujax.redirect_listener', FrujaxRedirectListener::class)
+    $services->set(FrujaxAnnotationListener::class)
         ->tag('kernel.event_subscriber');
 
-    $services->set('ruwork_frujax.response_listener', FrujaxResponseListener::class)
+    $services->set(FrujaxRedirectListener::class)
         ->tag('kernel.event_subscriber');
 
-    $services->set('ruwork_frujax.annotation_listener', FrujaxAnnotationListener::class)
+    $services->set(FrujaxResponseListener::class)
         ->tag('kernel.event_subscriber');
+
+    $services->set(FrujaxBlockAwareRenderer::class);
 };

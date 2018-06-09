a624ea3e942d285289d02c08f82c491586d2074d
Valentin <udaltsov.valentin@gmail.com>
2018-06-09 13:18:11 +0300

    [Reform] Added CheckboxTypeFalseValueExtension.

---
+++ src/Reform/Extension/CheckboxTypeFalseValueExtension.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ruwork\Reform\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class CheckboxTypeFalseValueExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefined('false_value')
+            ->setDefault('false_values', function (Options $options, $defaultFalseValues): array {
+                if (isset($options['false_value'])) {
+                    return [$options['false_value']];
+                }
+
+                return $defaultFalseValues ?? [];
+            })
+            ->setAllowedTypes('false_value', 'string');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        if (array_key_exists('false_value', $options)) {
+            $view->vars['false_value'] = $options['false_value'];
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return CheckboxType::class;
+    }
+}

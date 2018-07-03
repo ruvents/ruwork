b97dd1dbce642c2d205bc75dc80c187c38131fb2
Valentin <udaltsov.valentin@gmail.com>
2018-07-04 00:08:13 +0300

    [FrujaxBundle] Improved data_frujax().

--- src/FrujaxBundle/Twig/Extension/FrujaxExtension.php
+++ src/FrujaxBundle/Twig/Extension/FrujaxExtension.php
@@ -27,8 +27,14 @@ final class FrujaxExtension extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new TwigFunction('data_frujax', function (Environment $env, array $config): string {
-                return ' data-frujax="'.twig_escape_filter($env, json_encode($config), 'html_attr').'"';
+            new TwigFunction('data_frujax', function (Environment $env, array $config = []): string {
+                $attr = 'data-frujax';
+
+                if ([] !== $config) {
+                    $attr .= '="'.twig_escape_filter($env, json_encode($config), 'html_attr').'"';
+                }
+
+                return $attr;
             }, ['needs_environment' => true, 'is_safe' => ['html']]),
         ];
     }

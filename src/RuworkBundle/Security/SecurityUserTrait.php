8c5cc2e45f99068ca42e4ddac99d9001d58e6162
Valentin <udaltsov.valentin@gmail.com>
2018-09-13 02:20:38 +0300

    [RuworkBundle] Remove deprecated SecurityUserTrait::getRoles()

--- src/RuworkBundle/Security/SecurityUserTrait.php
+++ src/RuworkBundle/Security/SecurityUserTrait.php
@@ -9,15 +9,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 trait SecurityUserTrait
 {
     /**
-     * @deprecated Deprecated since 0.11.1 and will be removed in 0.12.0.
-     * @see        UserInterface::getRoles()
-     */
-    public function getRoles()
-    {
-        return ['ROLE_USER'];
-    }
-
-    /**
      * @see UserInterface::getPassword()
      */
     public function getPassword()

ceb53e35a05610428a3548a0175e24669e3c8f6b
Valentin <udaltsov.valentin@gmail.com>
2018-09-20 04:21:15 +0300

    [RuworkBundle] Remove Mailer from Extension

--- src/RuworkBundle/DependencyInjection/RuworkExtension.php
+++ src/RuworkBundle/DependencyInjection/RuworkExtension.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ruwork\RuworkBundle\DependencyInjection;
 
-use Ruwork\RuworkBundle\Mailer\Mailer;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
@@ -20,9 +19,5 @@ final class RuworkExtension extends ConfigurableExtension
         $locator = new FileLocator(__DIR__.'/../Resources/config');
         $loader = new PhpFileLoader($container, $locator);
         $loader->load('services.php');
-
-        $container
-            ->findDefinition(Mailer::class)
-            ->setArgument('$users', $config['mailer']['users']);
     }
 }
